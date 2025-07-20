465966eb9d09fbae4d4a78b4d3c364f92b3140ba
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-20 12:27:45 +0300

    Add project files.

---
+++ Week04BookExerciseMain/BookClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week04BookExerciseMain
+{
+    internal class BookClass
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished { get; set; }
+
+        public BookClass()   //Non-parameterized constructor
+        {
+            Title = "Unkown Title";
+            Author = "Unkown Author";
+            YearPublished = 0;
+
+        }
+
+        public BookClass(string title, string author, int yearPublished) //Parameterized constructor
+        {
+            Title = title;
+            Author = author;
+            YearPublished = yearPublished;
+        }
+
+        public void DisplayBookInfo()  //Method to display book information 
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Year Published: {YearPublished}");
+        }
+      
+    }
+}
---
+++ Week04BookExerciseMain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Week04BookExerciseMain;
+
+class Program
+{
+    static void Main(string[] args)
+        {
+    BookClass book1 = new BookClass("1984", "George Orwell", 1949); // Using the parameterized constructor
+    BookClass book2 = new BookClass("To Kill a Mockingbird", "Harper Lee", 1960);
+    BookClass book3 = new BookClass(); // Using the default constructor
+
+  book3.DisplayBookInfo(); // Display default book information
+  book1.DisplayBookInfo(); // Display book1 information
+  book2.DisplayBookInfo();
+    }
+
+
+}
